c718cc883c4fea42fa2127f225337dd1f870253d
yjw <yjw@qq.com>
2019-06-09 19:52:37 +0800

    修改

--- gitproject/src/gitTest.java
+++ gitproject/src/gitTest.java
@@ -1,5 +1,11 @@
 public class gitTest {
     public static void main(String[] args) {
         System.out.println("123");
+        System.out.println("321");
+        System.out.println("3");
+        System.out.println("2");
+        System.out.println("1");
+        System.out.println("3212");
     }
+
 }
